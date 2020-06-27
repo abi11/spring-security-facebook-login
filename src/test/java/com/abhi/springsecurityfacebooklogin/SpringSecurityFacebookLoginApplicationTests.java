1939b61e08edaf092a8fe6c366279c2d5c6b1862
abi11 <abhiyendra5@yahoo.com>
2020-06-26 22:28:00 -0400

    base commit for existing project

---
+++ src/test/java/com/abhi/springsecurityfacebooklogin/SpringSecurityFacebookLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.springsecurityfacebooklogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFacebookLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
